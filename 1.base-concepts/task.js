f2e89cc49549e08f6e00d8fcce8a50ee86edfece
Ekatherine Kuznetsova <sadilkerwi@gmail.com>
2025-04-10 22:44:59 +0300

    First Task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,22 +4,17 @@ function solveEquation(a, b, c) {
   let D = b ** 2 - 4 * a * c;
   let fstX, sndX;
 
-  if (D < 0) {
-    return arr;
-  } 
-  else if (D === 0) {
+  if (D === 0) {
     fstX = -b / (2 *a);
     arr.push(fstX);
-
-    return arr;
   } 
   else if (D > 0) {
     fstX = (-b + Math.sqrt(D) / (2 * a));
     sndX = (-b - Math.sqrt(D) / (2 * a));
     arr.push(fstX, sndX);
-
-    return arr;
   }
+
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
