1aae993c396666598dc510381f4decbed3b00246
Ekatherine Kuznetsova <sadilkerwi@gmail.com>
2025-04-10 23:12:22 +0300

    Оба задания выполнены

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,21 +2,26 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let D = b ** 2 - 4 * a * c;
-  let fstX, sndX;
+  let firstX, secondX;
 
   if (D === 0) {
-    fstX = -b / (2 *a);
-    arr.push(fstX);
+    firstX = -b / (2 *a);
+    arr.push(firstX);
   } 
   else if (D > 0) {
-    fstX = (-b + Math.sqrt(D) / (2 * a));
-    sndX = (-b - Math.sqrt(D) / (2 * a));
-    arr.push(fstX, sndX);
+    firstX = ((-b + Math.sqrt(D)) / (2 * a));
+    secondX = ((-b - Math.sqrt(D)) / (2 * a));
+    arr.push(firstX, secondX);
   }
 
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let monthPercent = percent / 100 / 12;
+  let creditBody = amount - contribution;
+  let monthPayment = creditBody * (monthPercent + (monthPercent / (((1 + monthPercent) ** countMonths) - 1)));
+  let total = parseFloat((monthPayment * countMonths).toFixed(2));
+
+  return total;
 }
